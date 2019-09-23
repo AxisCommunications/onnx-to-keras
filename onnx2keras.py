dfbdcf01c50bba0a438ab5f9a90f732a2d7dd3d9
Håkan x Ardö <hakanad@axis.com>
2019-09-23 16:38:55 +0200

    moved to custom repo
    Change-Id: I6332c78e306d56a451a5b50e2c6c773650c600f0

---
+++ onnx2keras.py
@@ -0,0 +1,259 @@
+from functools import partial
+
+import onnx
+from onnx import numpy_helper
+import tensorflow as tf
+from onnx.mapping import TENSOR_TYPE_TO_NP_TYPE
+import numpy as np
+
+
+class Operations:
+    def make_op(self, op_type, inputs, attrs):
+        print(op_type)
+        print([i.shape for i in inputs])
+        print(attrs)
+        print()
+        return getattr(self, 'op_' + op_type.lower())(*inputs, **attrs)
+
+class OnnxConstant: pass
+class InterleavedImageBatch: pass
+class VectorBatch: pass
+
+class Constant(np.ndarray):
+    data_format = OnnxConstant
+
+class TfKerasOperations(Operations):
+    keras = tf.keras
+
+    def make_constant(self, x):
+        return np.asarray(x).view(Constant)
+
+    def make_input(self, shape, dtype, name=None):
+        dtype = tf.as_dtype(dtype)
+        # XXX: Assumes all inputs are image batches that we want to transpose
+        assert len(shape) == 4
+        tensor = tf.keras.layers.Input((shape[2], shape[3], shape[1]), shape[0], name, dtype)
+        tensor.data_format = InterleavedImageBatch
+        return tensor
+
+    def op_conv(self, x, weights, bias=None, kernel_shape=None, strides=None, pads=None, dilations=None, group=None):
+        assert weights.data_format is OnnxConstant # XXX Assumes no ops on weights
+        assert bias is None or bias.data_format is OnnxConstant # XXX Assumes no ops on weights
+        if len(kernel_shape) == 2:
+            assert x.data_format is InterleavedImageBatch
+            assert kernel_shape == weights.shape[2:4]
+            if group == 1:
+                ConvClass = self.keras.layers.Conv2D
+            elif group == x.shape[3]:
+                def ConvClass(filters, kernel_size, strides, dilation_rate, padding,
+                              kernel_initializer, use_bias=True, bias_initializer='zeros'):
+                    return self.keras.layers.DepthwiseConv2D(kernel_size, strides, dilation_rate=dilation_rate,
+                                                             padding=padding, use_bias=use_bias,
+                                                             bias_initializer=bias_initializer,
+                                                             depthwise_initializer=kernel_initializer)
+            else:
+                raise NotImplementedError
+            if pads == (0,0,0,0):
+                padding = 'valid'
+            elif (kernel_shape[0] == kernel_shape[1] and pads[0] == pads[1] == pads[2] == pads[3] and
+                  pads[0] * 2 + 1 == kernel_shape[0] and strides == (1, 1) and dilations == (1, 1)):
+                padding = 'same'
+            else:
+                # ((top_pad, bottom_pad), (left_pad, right_pad))
+                pad = self.keras.layers.ZeroPadding2D(((pads[0], pads[2]), (pads[1], pads[3])))
+                x = pad(x)
+                padding = 'valid'
+
+            # Tf; filter_height, filter_width, in_channels, out_channels
+            # Torch: (out_channels, in_channels, kH, kW)
+            weights = weights.transpose(2, 3, 1, 0)
+            weights_initializer = self.keras.initializers.Constant(weights.view(np.ndarray))
+            if bias is None:
+                conv = ConvClass(weights.shape[3], kernel_shape, strides,
+                                 dilation_rate=dilations, padding=padding,
+                                 kernel_initializer=weights_initializer, use_bias=False)
+            else:
+                bias_initializer = self.keras.initializers.Constant(bias.view(np.ndarray))
+                conv = ConvClass(weights.shape[3], kernel_shape, strides,
+                                 dilation_rate=dilations, padding=padding,
+                                 kernel_initializer=weights_initializer, bias_initializer=bias_initializer)
+            out = conv(x)
+            out.data_format = InterleavedImageBatch
+            return [out]
+        else:
+            raise NotImplementedError
+
+    def op_relu(self, x):
+        out = self.keras.layers.ReLU()(x)
+        out.data_format = x.data_format
+        return [out]
+
+    def op_prelu(self, x, alpha):
+        assert alpha.data_format is OnnxConstant # XXX Assumes no ops on alpha
+        if len(alpha) == 1:
+            shared = list(range(1, len(x.shape)))
+            alpha = alpha.reshape((1,) * (len(x.shape) - 1))
+        elif len(alpha) == x.shape[-1]:
+            shared = list(range(1, len(x.shape) - 1))
+        else:
+            raise NotImplementedError
+        alpha_initializer = self.keras.initializers.Constant(alpha.view(np.ndarray))
+        out = self.keras.layers.PReLU(shared_axes=shared, alpha_initializer=alpha_initializer)(x)
+        out.data_format = x.data_format
+        return [out]
+
+    def op_maxpool(self, x, kernel_shape, pads, strides):
+        if len(kernel_shape) == 2:
+            assert x.data_format is InterleavedImageBatch
+            if pads == (0, 0, 0, 0):
+                padding = 'valid'
+            else:
+                raise NotImplementedError
+            out = self.keras.layers.MaxPool2D(kernel_shape, strides, padding)(x)
+            out.data_format = InterleavedImageBatch
+            return [out]
+        else:
+            raise NotImplementedError
+
+    def op_concat(self, *tensors, axis):
+        axis = (0, 3, 1, 2)[axis]
+        out = self.keras.layers.Concatenate(axis)(list(tensors))
+        for t in tensors:
+            assert tensors[0].data_format is t.data_format
+        out.data_format = tensors[0].data_format
+        return [out]
+
+    def op_convtranspose(self, x, weights, bias, kernel_shape, strides, pads, dilations, group):
+        assert weights.data_format is OnnxConstant # XXX Assumes no ops on weights
+        assert bias.data_format is OnnxConstant # XXX Assumes no ops on weights
+        if len(kernel_shape) == 2:
+            assert x.data_format is InterleavedImageBatch
+            assert kernel_shape == weights.shape[2:4]
+            if group != 1:
+                raise NotImplementedError
+            _, h_in, w_in, _ = x.shape
+            h_out = (h_in - 1) * strides[0] - 2 * pads[0] + dilations[0] * (kernel_shape[0] - 1) + 1 # FIXME: + output_padding[0]
+            w_out=(w_in - 1) * strides[1] - 2 * pads[1] + dilations[1] * (kernel_shape[1] - 1) + 1 # FIXME: + output_padding[1]
+
+            if pads == (0,0,0,0):
+                padding = 'valid'
+            elif h_out == strides[0] * h_in and w_out == strides[1] * w_in:
+                padding = 'same'
+            else:
+                raise NotImplementedError
+            # Tf; filter_height, filter_width, out_channels, in_channels
+            # Torch: (in_channels, out_channels, kH, kW)
+            weights = weights.transpose(2, 3, 1, 0)
+            weights_initializer = self.keras.initializers.Constant(weights.view(np.ndarray))
+            bias_initializer = self.keras.initializers.Constant(bias.view(np.ndarray))
+            conv = self.keras.layers.Conv2DTranspose(weights.shape[2], kernel_shape, strides,
+                                                     dilation_rate=dilations, padding=padding,
+                                                     kernel_initializer=weights_initializer,
+                                                     bias_initializer=bias_initializer)
+            out = conv(x)
+            assert out.shape[1] == h_out
+            assert out.shape[2] == w_out
+            out.data_format = InterleavedImageBatch
+            return [out]
+        else:
+            raise NotImplementedError
+
+    def op_batchnormalization(self, x, weight, bias, running_mean, running_var, momentum, epsilon):
+        if  len(x.shape) != 4:
+            raise NotImplementedError
+        moving_mean_initializer = self.keras.initializers.Constant(running_mean.view(np.ndarray))
+        moving_variance_initializer = self.keras.initializers.Constant(running_var.view(np.ndarray))
+        beta_initializer = self.keras.initializers.Constant(bias.view(np.ndarray))
+        gamma_initializer = self.keras.initializers.Constant(weight.view(np.ndarray))
+        norm = self.keras.layers.BatchNormalization(momentum=momentum, epsilon=epsilon,
+                                                    moving_mean_initializer=moving_mean_initializer,
+                                                    moving_variance_initializer=moving_variance_initializer,
+                                                    beta_initializer=beta_initializer,
+                                                    gamma_initializer=gamma_initializer)
+        out = norm(x)
+        out.data_format = x.data_format
+        return [out]
+
+    def op_unsqueeze(self, x, axes):
+        out = x
+        if isinstance(x, Constant):
+            for ax in axes:
+                out = np.expand_dims(out, ax)
+        else:
+            for ax in axes:
+                out = self.keras.layers.Lambda(lambda x: self.keras.backend.expand_dims(x, ax))(out)
+            out.data_format = None
+        return [out]
+
+    def op_clip(self, x, min, max):
+        clip = self.keras.layers.Lambda(lambda x: self.keras.backend.clip(x, min, max))
+        out = clip(x)
+        out.data_format = x.data_format
+        return [out]
+
+    def op_add(self, x1, x2):
+        assert x1.data_format == x2.data_format
+        out = self.keras.layers.Add()([x1, x2])
+        out.data_format = x1.data_format
+        return [out]
+
+    def op_reducemean(self, x, axes, keepdims):
+        assert x.data_format is InterleavedImageBatch
+        if axes == (2, 3) and keepdims == 0:
+            out = self.keras.layers.GlobalAveragePooling2D()(x)
+            out.data_format = VectorBatch
+        else:
+            raise NotImplementedError
+
+        return [out]
+
+    def op_gemm(self, x, weights, bias, beta, transB, alpha):
+        assert x.data_format is VectorBatch
+        if beta == 1.0 and transB == 1 and alpha == 1.0:
+            weights_initializer = self.keras.initializers.Constant(weights.view(np.ndarray).T)
+            bias_initializer = self.keras.initializers.Constant(bias.view(np.ndarray))
+            out = self.keras.layers.Dense(weights.shape[0], kernel_initializer=weights_initializer,
+                                          bias_initializer=bias_initializer)(x)
+            out.data_format = VectorBatch
+        else:
+            raise NotImplementedError
+        return [out]
+
+def parse_attr(a):
+    if a.type == onnx.AttributeProto.INT:
+        return a.i
+    elif a.type == onnx.AttributeProto.INTS:
+        return tuple(a.ints)
+    elif a.type == onnx.AttributeProto.FLOAT:
+        return a.f
+    else:
+        raise NotImplementedError
+
+
+def onnx2keras(onnx_model):
+    tensors = {}
+    ops = TfKerasOperations()
+
+    for init in onnx_model.graph.initializer:
+        tensors[init.name] = ops.make_constant(numpy_helper.to_array(init))
+
+    model_inputs = []
+    for input in onnx_model.graph.input:
+        if input.name in tensors:
+            continue
+        shape = [d.dim_value if (d.dim_value > 0 and d.dim_param == "") else None
+                 for d in input.type.tensor_type.shape.dim]
+        dtype = TENSOR_TYPE_TO_NP_TYPE[input.type.tensor_type.elem_type]
+        tensors[input.name] = ops.make_input(shape, dtype, input.name)
+        model_inputs.append(tensors[input.name])
+
+    for node in onnx_model.graph.node:
+        inputs = [tensors[i] for i in node.input]
+        attrs = {a.name: parse_attr(a) for a in node.attribute}
+        output_tensors = ops.make_op(node.op_type, inputs, attrs)
+        assert len(output_tensors) == len(node.output)
+        for n, t in zip(node.output, output_tensors):
+            tensors[n] = t
+
+    outputs = [tensors[o.name] for o in onnx_model.graph.output]
+    return tf.keras.models.Model(model_inputs, outputs)
---
+++ test_onnx2keras.py
@@ -0,0 +1,157 @@
+from io import BytesIO
+
+import onnx
+import torch.nn
+from torch.nn import Module
+from torchvision import models
+from numpy.testing import assert_almost_equal
+import numpy as np
+
+from onnx2keras import onnx2keras
+
+
+def make_onnx_model(net, indata):
+    fd = BytesIO()
+    torch.onnx.export(net, indata, fd)
+    fd.seek(0)
+    return onnx.load(fd)
+
+
+def convert_and_compare_output(net, indata, precition=6, image_out=True):
+    torch_indata = torch.tensor(indata)
+    y1 = net(torch_indata).detach().numpy()
+    onnx_model = make_onnx_model(net, torch.zeros_like(torch_indata))
+    kernas_net = onnx2keras(onnx_model)
+    y2 = kernas_net.predict(indata.transpose(0, 2, 3, 1))
+    if image_out:
+        y2 = y2.transpose(0, 3, 1, 2)
+    assert_almost_equal(y1, y2, precition)
+
+class GlobalAvgPool(Module):
+    def forward(self, x):
+        return x.mean([2, 3])
+
+
+class TestOnnx:
+    def test_conv(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 16, 7), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_conv_no_bias(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 16, 7, bias=False), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_conv_padding(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(1, 16, 3, padding=1), torch.nn.ReLU())
+        x = np.random.rand(1, 1, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_prelu(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 16, 7), torch.nn.PReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_prelu_per_channel(self):
+        act = torch.nn.PReLU(num_parameters=16)
+        act.weight[:] = torch.tensor(range(16))
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 16, 7), act)
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x, 5)
+
+    def test_maxpool(self):
+        net = torch.nn.Sequential(torch.nn.MaxPool2d(2))
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_concat(self):
+        for axis in range(1,4):
+            class Dbl(torch.nn.Module):
+                def forward(self, x):
+                    return torch.cat((x, x), axis)
+            x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+            convert_and_compare_output(Dbl(), x)
+
+    def test_conv_transpose(self):
+        net = torch.nn.Sequential(torch.nn.ConvTranspose2d(3, 16, 5, 2), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 112, 112).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_conv_transpose_padding(self):
+        net = torch.nn.Sequential(torch.nn.ConvTranspose2d(3, 16, 4, 2, padding=1), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 112, 112).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_conv_different_padding(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=(3, 4)))
+        x = np.random.rand(1, 3, 384, 544).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_conv_stride2_padding(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3))
+        x = np.random.rand(1, 3, 384, 544).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_batchnorm(self):
+        bn = torch.nn.BatchNorm2d(3)
+        bn.running_mean.uniform_()
+        bn.running_var.uniform_()
+        net = torch.nn.Sequential(bn, torch.nn.ReLU())
+        net.eval()
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_clamp(self):
+        class Clamp(Module):
+            def forward(self, x):
+                return torch.clamp(x, 0.3, 0.7)
+        net = torch.nn.Sequential(torch.nn.ReLU(), Clamp(), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_depthwise(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 3, 7, groups=3), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_depthwise_no_bias(self):
+        net = torch.nn.Sequential(torch.nn.Conv2d(3, 3, 7, groups=3, bias=False), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_add(self):
+        class AddTst(Module):
+            def __init__(self):
+                Module.__init__(self)
+                self.conv1 = torch.nn.Conv2d(3, 3, 7)
+                self.conv2 = torch.nn.Conv2d(3, 3, 7)
+            def forward(self, x):
+                return self.conv1(x).relu_() + self.conv2(x).relu_()
+        net = torch.nn.Sequential(AddTst(), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x)
+
+    def test_global_avrage_pooling(self):
+        net = torch.nn.Sequential(GlobalAvgPool(), torch.nn.ReLU())
+        x = np.random.rand(1, 3, 16, 16).astype(np.float32)
+        convert_and_compare_output(net, x, image_out=False)
+
+    def test_dropout(self):
+        net = torch.nn.Sequential(GlobalAvgPool(), torch.nn.Dropout(), torch.nn.ReLU())
+        net.eval()
+        x = np.random.rand(1, 3, 16, 16).astype(np.float32)
+        convert_and_compare_output(net, x, image_out=False)
+
+    def test_linear(self):
+        net = torch.nn.Sequential(GlobalAvgPool(), torch.nn.Linear(3, 8), torch.nn.ReLU())
+        net.eval()
+        x = np.random.rand(5, 3, 16, 16).astype(np.float32)
+        convert_and_compare_output(net, x, image_out=False)
+
+    def test_mobilenet_v2(self):
+        net = models.mobilenet_v2()
+        net.eval()
+        x = np.random.rand(1, 3, 224, 224).astype(np.float32)
+        convert_and_compare_output(net, x, image_out=False)
+
